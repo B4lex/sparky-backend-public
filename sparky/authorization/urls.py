76984dde6aa22504fc491c044e87dee534516c61
Oleksandr Blakytnyi <oblakytnyi@gmail.com>
2023-11-08 07:20:41 +0100

    [SP-13] Add logout endpoint Update UserSerializer fields

--- sparky/authorization/urls.py
+++ sparky/authorization/urls.py
@@ -1,7 +1,11 @@
+from dj_rest_auth.views import LogoutView
 from django.urls import path
 
 from sparky.authorization import views
 
 app_name = "auth"
 
-urlpatterns = [path("google/", views.GoogleOAuthLoginView.as_view(), name="google")]
+urlpatterns = [
+    path("google/", views.GoogleOAuthLoginView.as_view(), name="google"),
+    path("logout/", LogoutView.as_view(), name="logout")
+]
